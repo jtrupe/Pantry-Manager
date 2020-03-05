6525daf469d03835cd3e6e094f6c22bfa73dfe5c
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-03-05 15:05:55 -0800

    passport.js authentication

---
+++ backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://localhost/Final-Project",
+    secretOrKey: "secret"
+};
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // app.use(routes);
+const db = require("./config/keys").mongoURI;
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Final-Project';
 

46ebd2188fa532093b1211d88a4503a4342a494c
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-03-09 18:18:26 -0700

    pantry page

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Pantry from "../../../src/Pages/pantry"
 class Dashboard extends Component {
     onLogoutClick = e => {
         e.preventDefault();
@@ -34,31 +35,7 @@ class Dashboard extends Component {
             </button>
                     </div>
                 </div>
-                <div className="title text-center mb-0 py-3">
-                    <h1 className="display-4 text-center m-2">Pantry</h1>
-                </div>
-
-                <div className="row">
-
-                    <div className="col-sm-6 pt-3 add">
-                        <h2 className="text-center"><u>Add Ingredients</u></h2>
-                        <input type="text" id="ingredient-search" class="form-control" placeholder="start typing to find your ingredient" />
-                        <div className="returned-search-items mt-4"></div>
-                    </div>
-
-                    <div className="myIngredients col-sm-6 pt-3">
-                        <h2 className="text-center mb-4"><u>My Ingredients</u></h2>
-                        <div class="ingredient-list"></div>
-                        {/* {{#each data}} */}
-                        <div className="pantry-item ml-3 font-weight">
-                            Item Name: {}
-                            <button value="{ }"
-                                className="pantry-item-remove text-center btn btn-outline-dark pl-2 pr-1 float-right"><span role="img" aria-label="x">❌</span></button>
-                            <hr />
-                        </div>
-                        {/* {{/ each}} */}
-                    </div>
-                </div>
+          <Pantry />
             </div>
         );
     }
---
+++ client/src/Pages/pantry.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { render } from "react-dom";
+
+class Pantry extends Component {
+
+    render() {
+        return (
+            <div className="container">
+
+                <div className="title text-center mb-0 py-3">
+                    <h1 className="display-4 text-center m-2">Pantry</h1>
+                </div>
+
+                <div className="row">
+
+                    <div className="col-sm-6 pt-3 add">
+                        <h2 className="text-center"><u>Add Ingredients</u></h2>
+                        <input type="text" id="ingredient-search" class="form-control" placeholder="start typing to find your ingredient" />
+                        <div className="returned-search-items mt-4"></div>
+                    </div>
+
+                    <div className="myIngredients col-sm-6 pt-3">
+                        <h2 className="text-center mb-4"><u>My Ingredients</u></h2>
+                        <div class="ingredient-list"></div>
+                        {/* {{#each data}} */}
+                        <div className="pantry-item ml-3 font-weight">
+                            Item Name: {}
+                            <button value="{ }"
+                                className="pantry-item-remove text-center btn btn-outline-dark pl-2 pr-1 float-right"><span role="img" aria-label="x">❌</span></button>
+                            <hr />
+                        </div>
+                        {/* {{/ each}} */}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Pantry; 

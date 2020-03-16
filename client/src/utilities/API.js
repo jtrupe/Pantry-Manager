c0f9eb76ee5976e6077109deb4a35720b319d392
Julian Trupe <juliantrupe@gmail.com>
2020-03-15 21:58:51 -0700

    oops

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -7,24 +7,24 @@ export default {
 	findOrCreateIngredient: (userId) => {
 			return axios.post(`/api/pantry/${userId}`);
 	},
-	findFavorites: (userId) => {
-		return axios.get(`/api/favorites/${userId}`);
-	},
-	addFavorite: (userId) => {
-		return axios.put(`/api/favorites/${userId}`);
-	},
-	deleteFavorite: (userId) => {
-		return axios.put(`/api/favorites/delete/${userId}`);
+	// findFavorites: (userId) => {
+	// 	return axios.get(`/api/favorites/${userId}`);
+	// },
+	// addFavorite: (userId) => {
+	// 	return axios.put(`/api/favorites/${userId}`);
+	// },
+	// deleteFavorite: (userId) => {
+	// 	return axios.put(`/api/favorites/delete/${userId}`);
 	},
 	getAllIngredients: () => {
 		return axios.get(`/api/pantry/`);
 	},
-	updatePantryItem: (userId) => {
-		return axios.put(`/api/pantry/update/${userId}`);
-	},
-	deletePantryItem: (userId) => {
-		return axios.put(`/api/pantry/delete/${userId}`);
-	},
+	// updatePantryItem: (userId) => {
+	// 	return axios.put(`/api/pantry/update/${userId}`);
+	// },
+	// deletePantryItem: (userId) => {
+	// 	return axios.put(`/api/pantry/delete/${userId}`);
+	// },
 	ingredientSearch: () => {
 		return axios.get('/api/spoon/ingredient-search');
 	},

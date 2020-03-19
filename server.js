eecbe6a3405a3ba8d0ada23092c41ef4fea58a30
Julian Trupe <juliantrupe@gmail.com>
2020-03-19 12:25:55 -0700

    redeploy again

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ const db = require("./config/keys").mongoURI;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://admin:7KZaMWL%4inP@ds015849.mlab.com:15849/heroku_m1kcxm85";
 console.log(MONGODB_URI);
 
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static('client/build'));
+}
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 	.then(() => console.log("MongoDB connected successfully"))
 	.catch(err => console.log(err));
-
 // Passport middleware
 app.use(passport.initialize());
 // Passport config

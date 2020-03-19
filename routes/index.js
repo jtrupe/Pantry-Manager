ed0bf1d17cd47dcd06a4303b879e73996eb3ea93
Julian Trupe <juliantrupe@gmail.com>
2020-03-19 16:35:57 -0700

    maybe take 8

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./api');
 app.use('/api', apiRoutes);
 
 app.use((req,res) => {
-    res.sendFile(path.join(__dirname, '../../../client/build/index.html'));
+    res.sendFile(path.join(__dirname), '../client/build/index.html');
 });
 
 module.exports = app;
